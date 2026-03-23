0b48bdda5b791eadd00518e6c34148b50622cada
Njmcmil <Njmcmil@clemson.edu>
2026-03-23 18:47:51 -0400

    Initial Commit

---
+++ Ambient Health Interface/Ambient_Health_InterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ambient_Health_InterfaceApp.swift
+//  Ambient Health Interface
+//
+//  Created by Nathan Mcmillan on 3/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Ambient_Health_InterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ambient Health Interface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ambient Health Interface
+//
+//  Created by Nathan Mcmillan on 3/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
